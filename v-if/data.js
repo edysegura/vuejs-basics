8e69d20fda4b30d6a50e35d134126c27c821d019
Edy Segura <edysegura@gmail.com>
2019-03-25 22:45:44 -0300

    Add example with v-if
    This closes #1

---
+++ v-if/data.js
@@ -0,0 +1,14 @@
+export default [
+  { name: 'Alison Marinho', isPlus2: false },
+  { name: 'Edy Segura', isPlus2: true },
+  { name: 'Leidiane Cunha', isPlus2: false },
+  { name: 'Lidiane Segura', isPlus2: false },
+  { name: 'Márcio Rotella', isPlus2: false },
+  { name: 'Nathalia Barbosa', isPlus2: false },
+  { name: 'Rafael Paes', isPlus2: false },
+  { name: 'Rafael Pedrosa', isPlus2: false },
+  { name: 'Rinaldo Alípio Bueno', isPlus2: false },
+  { name: 'Rodolfo Zahn', isPlus2: true },
+  { name: 'Sandro Oliveira', isPlus2: true },
+  { name: 'Wanderval Carvalho', isPlus2: false }
+]
---
+++ v-if/index.js
@@ -0,0 +1,8 @@
+import data from './data.js'
+
+const myApp = new Vue({
+  el: '#my-app',
+  data: {
+    members: data
+  }
+})
