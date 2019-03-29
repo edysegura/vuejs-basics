0042dbecf1cd9286dd09fe2530d1b9ec7ba9ac80
Edy Segura <edysegura@gmail.com>
2019-03-28 22:08:55 -0300

    Refactor index.js for vue component example

--- vue-component/index.js
+++ vue-component/index.js
@@ -23,7 +23,9 @@ const myApp = new Vue({
     groceryList: [
       { id: 0, text: 'Vegetables' },
       { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
+      { id: 2, text: 'Rice' },
+      { id: 3, text: 'Beans' },
+      { id: 4, text: 'Whatever else humans are supposed to eat' },
     ]
   }
 })
